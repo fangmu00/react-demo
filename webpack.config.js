efe01e1a9b99560189ba318ff12ddf89d17cb891
Sun Guo <wb-bysunguo@alibaba-inc.com>
2017-05-16 16:52:40 +0800

    'build'

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,16 @@ module.exports = {
             use: [ 'style-loader', 'css-loader' ]
         },
         {
+            test: /\.less$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "less-loader" // compiles Less to CSS
+            }]
+        },
+        {
             test: /\.hbs$/,
             loader: "handlebars-loader"
         }]
