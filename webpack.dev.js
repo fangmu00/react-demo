a557c2ac08e3a9a138c72a8ad347fa8c1cf21e80
Sun Guo <wb-bysunguo@alibaba-inc.com>
2017-06-23 11:35:52 +0800

    '新增entry文件'

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,26 +2,11 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { resolve } = path;
+const entry = require('./entry');
 
 module.exports = {
     context: resolve(__dirname, 'src'),
-    entry: [
-        'react-hot-loader/patch',
-        // 开启 React 代码的模块热替换(HMR)
-
-        'webpack-dev-server/client?http://localhost:8080',
-        // 为 webpack-dev-server 的环境打包代码
-        // 然后连接到指定服务器域名与端口
-
-        'webpack/hot/only-dev-server',
-        // 为热替换(HMR)打包好代码
-        // only- 意味着只有成功更新运行代码才会执行热替换(HMR)
-
-
-        './app/app.js'
-        // 我们 app 的入口文件
-
-    ],
+    entry,
     output: {
         path: path.join(__dirname,'dist'),
         filename: '[name].js',
